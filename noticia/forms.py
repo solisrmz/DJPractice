e96ec936495d7ec77cf49e9cce6b0129aade98d5
Solis <imsolis@hotmail.com>
2020-06-30 13:00:09 -0500

    Validacion personalizada en name y email

--- noticia/forms.py
+++ noticia/forms.py
@@ -6,10 +6,18 @@ class FormRegModelForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ["nombre", "email"]
-        
+
     def clean_email(self):
-        email = self.cleaned_data
+        email = self.cleaned_data.get("email")
+        #Para validar estructura del email
+        email_base, proveeder = email.split("@")
+        dominio, extension = proveeder.split(".")
+        if not extension == "com":
+            raise forms.ValidationError("Por favor ingresa una extension .com en tu email")
         return email
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get("nombre")
+        return nombre
 
 
 #Datos que se van a necesitar en dicho formulario
